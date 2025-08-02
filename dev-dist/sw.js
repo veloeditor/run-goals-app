74aaa9741a9fb8d4c333a79bf8fe0e433d2f7cc3
Brian Wilson <wilsonedit@gmail.com>
2025-08-02 10:36:03 -0500

    ..

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ce4f0d5f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gts8ik1usa"
+    "revision": "0.3bhms3k5c6"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
